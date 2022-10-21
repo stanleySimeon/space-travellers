89a85e2f4b1beeb69bf016cd472876160eb2cc70
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-10-21 04:08:49 +0100

    Update Profile

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -3,7 +3,7 @@ import JoinedMissions from './joinedMissions';
 import ReservedRockets from './reservedRockets';
 
 const Profile = () => (
-  <div className="container-fluid px-2 md:px-24 py-20 md:py-32 h-screen flex justify-between gap-2">
+  <div className="container-fluid px-2 md:px-16 lg:px-32 py-20 md:py-32 h-screen flex justify-between gap-2">
     <JoinedMissions />
     <ReservedRockets />
   </div>
