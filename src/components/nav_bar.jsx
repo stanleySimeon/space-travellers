03b29a5ead888cf36f82bfb942e1d4f895ffdab2
mstanley.me@gmail.com <mstanley.me@gmail.com>
2023-02-10 16:26:24 -0500

    Update navbar

--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -28,7 +28,7 @@ export default function Navbar() {
         <img src={planet} alt="logo" className="w-6 h-6 md:h-8 md:w-8 lg:h-14 lg:w-14" />
         <h1 className="hidden lg:inline text-white">Space Travelers&apos; Hub</h1>
       </Link>
-      <ul className="hidden mobile-menu absolute w-full md:w-auto  top-12 pt-44 md:top-0 left-0 h-screen bg-opacity-95 md:h-auto bg-black md:relative md:flex md:gap-32 lg:gap-14 text-white font-light md:font-normal">
+      <ul className="hidden mobile-menu absolute w-full md:w-auto  top-12 pt-44 md:pt-0 md:top-0 left-0 h-screen bg-opacity-95 md:h-auto bg-black md:relative md:flex md:gap-32 lg:gap-14 text-white font-light md:font-normal">
         {navLinks.map((link) => (
           <li
             key={link.id}
